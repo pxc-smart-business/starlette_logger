c4825071e5b23e0e2ff2f279e91384e2fb699053
Sebastian Kowalak <skowalak@phoenixcontact-sb.io>
2023-08-02 16:51:33 +0200

    fix: more tests, await logging calls

--- tests/test_request_id_middleware.py
+++ tests/test_request_id_middleware.py
@@ -1,15 +1,31 @@
 from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.testclient import TestClient
 import pytest
 import starlette
 import starlette_logger
+import structlog
 
-class TestRequestIdMiddleware:
+structlog.configure()
+
+class TestMiddleware:
 
     @pytest.fixture
     def testapp(self):
         app = Starlette()
+        app.add_route("/health", lambda *args: JSONResponse({}))
         yield app
 
-    def test_init(self, testapp):
+    @pytest.fixture
+    def testclient(self, testapp: Starlette):
+        yield TestClient(testapp)
+
+    def test_init_idware(self, testapp: Starlette, testclient: TestClient):
         testapp.add_middleware(starlette_logger.RequestIdMiddleware)
+        testclient.get("/health")
+
+    def test_init_logware(self, testapp: Starlette, testclient: TestClient):
+        testapp.add_middleware(starlette_logger.RequestLoggerMiddleware)
+        testclient.get("/health")
+
 
